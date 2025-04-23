8a20b6358ac25d8b81cac97eeb731aff62275644
Felipe Rodrigues Dummer <dumirdumir06@gmail.com>
2025-04-23 11:05:55 -0300

    Add files via upload

---
+++ ex_5-1.c
@@ -0,0 +1,35 @@
+/*
+    As written, getint treats a + or - not followed by a digit as a valid representation of zero. Fix it to push such a character
+    back on the input;
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int getint(int *pn)
+{
+    int c, sign;
+
+    while (isspace(c = getch()))
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-') {
+        int temp = getch();
+        if (!isdigit(temp)) {
+            ungetch(temp);
+            ungetch(c);
+            return -1;
+        }
+        c = temp;
+    }
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+            ungetch(c);
+    return c;
+}
