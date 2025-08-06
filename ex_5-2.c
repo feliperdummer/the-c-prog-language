1a98deb58134be1d58938c057243983fd49acaef
Felipe Rodrigues Dummer <dumirdumir06@gmail.com>
2025-08-06 16:12:41 -0300

    Update ex_5-2.c
    Só deixei o código um pouco mais robusto, cobrindo inputs como: 
    
    +.5 (numero com ponto decimal antes do digito)
    12. (ponto decimal sem digito após)
    + (apenas o sinal)
    123 (sem pono decimal)

--- ex_5-2.c
+++ ex_5-2.c
@@ -1,42 +1,49 @@
 /*
-    Write getfloat, the floating-point analog of getint. What type does getfloat return as its function value ?
+    As written, getint treats a + or - not followed by a digit as a valid representation of zero. Fix it to push such a character
+    back on the input;
 */
 
 #include <stdio.h>
 #include <ctype.h>
 
-int getfloat(double *pn)
-{
-    int c, sign, count = 1;
-    double dec = 0;
+int getfloat(float *pn) {
+    int c, sign;
 
     while (isspace(c = getch()))
         ;
-    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+    if (!isdigit(c) && c != '+' && c != '-' && c != EOF) {
         ungetch(c);
         return 0;
     }
     sign = (c == '-') ? -1 : 1;
-    if (c == '+' || c == '-') {
+    if (c == '-' || c == '+') {
         int temp = getch();
         if (!isdigit(temp)) {
-            ungetch(temp);
             ungetch(c);
-            return -1;
+            ungetch(temp);
+            return 0;
         }
         c = temp;
     }
     for (*pn = 0; isdigit(c); c = getch())
         *pn = 10 * *pn + (c - '0');
     if (c == '.') {
-        double frac = 0, div = 10;
+        float dec = 0, power = 10.0;
+        int temp = c;
         while (isdigit(c = getch())) {
-            frac += (c - '0') / 10;
-            div *= 10;
+            dec += (c - '0') / power;
+            power *= 10;
         }
-        dec = frac;
+        if (dec == 0) {
+            ungetch(temp);
+            if (c != EOF)
+                ungetch(c);
+        }
+        else
+            *pn += dec;
     }
-    *pn += dec;
+    else
+        ungetch(c);
     *pn *= sign;
     if (c != EOF)
         ungetch(c);
